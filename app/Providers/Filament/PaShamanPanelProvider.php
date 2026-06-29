29dda11ee78713cf0cee635382396d9b0b35e016
paShaman <nikitin696@gmail.com>
2026-06-29 11:31:29 +0300

    Replace original_request with request helper in MoneyResource and add favicon to panel

--- app/Providers/Filament/PaShamanPanelProvider.php
+++ app/Providers/Filament/PaShamanPanelProvider.php
@@ -35,6 +35,7 @@ class PaShamanPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->favicon(asset('images/favicon.svg'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
