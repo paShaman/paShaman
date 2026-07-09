b6c75d727d711e742e0725eafd40a43b865df102
paShaman <nikitin696@gmail.com>
2026-07-09 16:43:46 +0300

    Tweak banner blob width

--- resources/js/Components/shared/AppBanner.vue
+++ resources/js/Components/shared/AppBanner.vue
@@ -5,7 +5,7 @@
     <section class="relative flex flex-col items-center justify-center text-center pt-12 sm:pt-16 pb-20 sm:pb-28">
         <!-- Background blob -->
         <div
-            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-3/5 w-125 sm:w-180 aspect-square rounded-full bg-accent-terracotta/10 animate-blob pointer-events-none"
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-3/5 w-115 sm:w-180 aspect-square rounded-full bg-accent-terracotta/10 animate-blob pointer-events-none"
             aria-hidden="true"
         />
 
